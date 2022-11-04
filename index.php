a924314cbb2b47ef846d3f05607d74044ec33682
Matheusbrasil19 <matheusbrasil862@gmail.com>
2022-11-04 19:44:17 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>exer5</title>
+ </head>
+ <body>
+ <form action='exer5.php' method="get">
+  Nota 1: <input type="text" name="num1"><br>
+  Nota 2: <input type="text" name="num2"><br>
+  Nota 3: <input type="text" name="num3"><br>
+  <input type="submit">
+ </form>
+ </body>
+</html>
